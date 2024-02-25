275da0d00d69bcbeec2e2d77a9ff9c449a4b6ce0
IamSummer72 <84401760+IamSummer72@users.noreply.github.com>
2024-02-25 22:56:51 +0900

    Add game instance

---
+++ Source/ExtendedFramework/ExtendedGameInstance.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/GameInstance.h"
+#include "ExtendedGameInstance.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class EXTENDEDFRAMEWORK_API UExtendedGameInstance : public UGameInstance
+{
+	GENERATED_BODY()
+	
+};

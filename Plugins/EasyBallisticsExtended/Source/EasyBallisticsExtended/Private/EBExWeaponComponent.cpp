c581cb0d710fd819eef4e156a5e79e2da9f9655f
IamSummer72 <84401760+IamSummer72@users.noreply.github.com>
2024-02-25 21:57:03 +0900

    Add WeaponComponent

---
+++ Plugins/EasyBallisticsExtended/Source/EasyBallisticsExtended/Private/EBExWeaponComponent.cpp
@@ -0,0 +1,40 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "EBExWeapon.h"
+#include "EBExWeaponComponent.h"
+
+// Sets default values for this component's properties
+UEBExWeaponComponent::UEBExWeaponComponent()
+{
+	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
+	// off to improve performance if you don't need them.
+	PrimaryComponentTick.bCanEverTick = true;
+
+}
+
+
+// Called when the game starts
+void UEBExWeaponComponent::BeginPlay()
+{
+	Super::BeginPlay();
+
+	// Spawn the weapon
+	if (WeaponClass)
+	{
+		FActorSpawnParameters SpawnParams;
+		SpawnParams.Owner = GetOwner();
+
+		TObjectPtr<AEBExWeapon> Weapon = GetWorld()->SpawnActor<AEBExWeapon>(WeaponClass, GetComponentTransform(), SpawnParams);
+		Weapon->AttachToComponent(this, FAttachmentTransformRules::SnapToTargetNotIncludingScale);
+	}
+}
+
+
+// Called every frame
+void UEBExWeaponComponent::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
+{
+	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+
+	// ...
+}
+
---
+++ Plugins/EasyBallisticsExtended/Source/EasyBallisticsExtended/Public/EBExWeaponComponent.h
@@ -0,0 +1,32 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/SceneComponent.h"
+#include "EBExWeaponComponent.generated.h"
+
+class AEBExWeapon;
+
+UCLASS( ClassGroup=(Custom), meta=(BlueprintSpawnableComponent), DisplayName = "WeaponComponent")
+class EASYBALLISTICSEXTENDED_API UEBExWeaponComponent : public USceneComponent
+{
+	GENERATED_BODY()
+
+public:	
+	// Sets default values for this component's properties
+	UEBExWeaponComponent();
+
+protected:
+	// Called when the game starts
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
+
+	// The weapon class to spawn
+	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category = "Weapon")
+	TSubclassOf<class AEBExWeapon> WeaponClass;
+
+};
